c882e9fbd08216fdb67fa511da22f34cc3ca0e8d
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-03-19 14:30:05 -0400

    Wrote fourth and last endpoint for postRouter, PUT request

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -35,8 +35,24 @@ router.delete("/:id", validatePostId, (req, res) => {
 		});
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", validatePostId, (req, res) => {
 	// do your magic!
+	const { id } = req.post;
+	const updatedPost = { ...req.post, text: req.body.text };
+
+	console.log("req.post", req.post);
+	console.log("req.body", req.body);
+
+	!req.body.text
+		? res.status(400).json({ message: "Please provide text for the post" })
+		: postDb
+				.update(id, updatedPost)
+				.then(updated => {
+					res.status(200).json(updatedPost);
+				})
+				.catch(error => {
+					res.status(500).json({ message: "Error updating the post", error });
+				});
 });
 
 // custom middleware

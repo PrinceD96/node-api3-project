8dc7a7e02698f2d05d0d3dd733f4024d8c14f421
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-03-18 20:10:37 -0400

    Added logger middleware

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 
 server.use(express.json());
 server.use(cors());
+server.use(logger);
 
 server.get("/", (req, res) => {
 	res.send(`<h2>Let's write some middleware!</h2>`);
@@ -13,6 +14,17 @@ server.get("/", (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+	console.log(
+		"logger",
+		req.method,
+		req.originalUrl,
+		new Date()
+			.toISOString()
+			.slice(0, 19)
+			.replace("T", " ")
+	);
+	next();
+}
 
 module.exports = server;
